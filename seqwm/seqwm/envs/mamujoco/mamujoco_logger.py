5e5fcd6a23601ebd4dc4dba803b196154eadc267
Haodong <haodoz0118@gmail.com>
2026-04-01 21:38:52 -0400

    Deleted unnecessary thing

--- seqwm/seqwm/envs/mamujoco/mamujoco_logger.py
+++
@@ -1,6 +0,0 @@
-from seqwm.common.base_logger import BaseLogger
-
-
-class MAMuJoCoLogger(BaseLogger):
-    def get_task_name(self):
-        return f"{self.env_args['scenario']}-{self.env_args['agent_conf']}"
--- seqwm/seqwm/runners/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Runner registry."""
-from seqwm.runners.world_model_runner import WorldModelRunner
-
-RUNNER_REGISTRY = {
-    "seqwm": WorldModelRunner,
-}
